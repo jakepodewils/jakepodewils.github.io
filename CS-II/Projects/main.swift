f92fa3196919368367ec5d3b66afa5d51b748b64
jake podewils <73614707+jakepodewils@users.noreply.github.com>
2022-09-16 22:16:00 -0500

    Update main.swift

--- CS-II/Projects/main.swift
+++ CS-II/Projects/main.swift
@@ -1,4 +1,17 @@
-import Foundation
-    var users = ["pineapple", "apple", "green", "street"]
-    users.sort()
-    print(users)
+func readLines() -> [String] {                                                                                                                                                                               
+    var strArray = [String]();                                                                                                                                                                               
+    print("ALPHABETICAL SORTER V1.1")                                                                                                                                                                        
+    print("Input a word to sort. If finished, just exit the program, and your list will be sorted for you.")                                                                                                 
+    while let x = readLine() {                                                                                                                                                                               
+        strArray.append(x);                                                                                                                                                                                  
+    }                                                                                                                                                                                                        
+    strArray.sort()                                                                                                                                                                                          
+    return strArray                                                                                                                                                                                          
+}                                                                                                                                                                                                            
+func printLines(lines: [String]) {                                                                                                                                                                           
+    for x in lines {                                                                                                                                                                                         
+        print(x)                                                                                                                                                                                             
+        var sorted = x.sorted()                                                                                                                                                                              
+    }                                                                                                                                                                                                        
+}                                                                                                                                                                                                              
+print(readLines())  
