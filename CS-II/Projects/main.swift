2c0612a25b9e506ab026087c11e3860ec954177b
Jake Podewils <jake.podewils@student.allenisd.org>
2022-11-16 09:55:27 -0600

    lol

--- CS-II/Projects/main.swift
+++ CS-II/Projects/main.swift
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-func readLines() -> [String] {
-    var strArray = [String]();
-    print("ALPHABETICAL SORTER V1.1")
-    print("Input a word to sort. If finished, just exit the program, and your list will be sorted for you.")
-    while let x = readLine() {
-        strArray.append(x);
-    }
-    strArray.sort()
-    return strArray
-}
-func printLines(lines: [String]) {
-    for x in lines {
-        print(x)
-        var sorted = x.sorted()
-    }
-}
-print(readLines())
-
-=======
 func readLines() -> [String] {                                                                                                                                                                               
     var strArray = [String]();                                                                                                                                                                               
     print("ALPHABETICAL SORTER V1.1")                                                                                                                                                                        
@@ -34,5 +14,4 @@ func printLines(lines: [String]) {
         var sorted = x.sorted()                                                                                                                                                                              
     }                                                                                                                                                                                                        
 }                                                                                                                                                                                                              
-print(readLines())  
->>>>>>> f92fa3196919368367ec5d3b66afa5d51b748b64
+print(readLines()) 
